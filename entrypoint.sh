31278a03e4e7036f4ecab55090f34a4c7f488590
veekee <docker@veekee.net>
2016-09-17 11:20:42 +0200

    Default entry point with apache modules and infinite loop (to allow graceful reload)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-if [ -z "$ENABLE_SSL" ]; then
-    echo 'Enabling SSL support'
-    /usr/sbin/a2ensite default-ssl
-    /usr/sbin/a2enmod ssl
+if [ ! -z "$ENABLE_SSL" ]; then
+	echo 'Enabling SSL support'
+	/usr/sbin/a2ensite default-ssl
+	/usr/sbin/a2enmod ssl
 fi
+
+echo "###########################################"
+date
+echo "###########################################"
+
+echo "Copying apache modules"
+cp /etc/apache2/mods-available/proxy* /etc/apache2/mods-enabled
+cp /etc/apache2/mods-available/vhost_alias.load /etc/apache2/mods-enabled
+cp /etc/apache2/mods-available/request.load /etc/apache2/mods-enabled
+cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled
+cp /etc/apache2/mods-available/xml2enc.load /etc/apache2/mods-enabled
+cp /etc/apache2/mods-available/slotmem_shm.load /etc/apache2/mods-enabled
+
+echo "Checking configuration"
+apache2ctl -t
+
+echo "Starting httpd deamon"
+service apache2 start
+
+while true
+do
+	sleep 30
+done
